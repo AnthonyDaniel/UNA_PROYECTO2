48d61d5e40f57094a4248db9f8bdd927dec77f83
Anthony Marín Bolívar <anthonymmarinbolivar@gmail.com>
2018-05-11 19:18:06 -0600

    Graphical interface improvements

--- src/view/MainGraphicInterface.java
+++ src/view/MainGraphicInterface.java
@@ -25,13 +25,22 @@ public class MainGraphicInterface {
     private Image newImagen; // icon of Jframe
 	private Font font; 
 	private JPanel subG;
-	
+	private JPanel subG2;
+	private JPanel subG3;
+	private JPanel subG4;
+	private JPanel subG5;
+	private JPanel subG6;
     
 	public MainGraphicInterface() {
 		
 		imagenFile = new File("src/icon.PNG");
 		font = new Font("Calibri",3,20); 
-		
+		subG = new JPanel();
+		subG2 = new JPanel();
+		subG3 = new JPanel();
+		subG4 = new JPanel();
+		subG5 = new JPanel();
+		subG6 = new JPanel();
 	}
 	
 	public void main() {
@@ -111,13 +120,13 @@ public class MainGraphicInterface {
 			
 			public void actionPerformed(ActionEvent arg0) {
 				
+				graphic.remove(subG2);
+				graphic.remove(subG3);	
+				graphic.remove(subG4);	
+				graphic.remove(subG5);	
+				graphic.remove(subG6);	
+				customerAdministration();
 				
-				if(panelOpen(medicalAdministration())) {
-					medicalAdministration().setVisible(false);
-					graphic.add(customerAdministration()).repaint();
-				}else {
-					graphic.add(customerAdministration()).repaint();
-				}
 				
 			}
 		});;
@@ -128,12 +137,14 @@ public class MainGraphicInterface {
 			public void actionPerformed(ActionEvent arg0) {
 
 				
-				if(panelOpen(customerAdministration())) {
-					customerAdministration().setVisible(false);
-					graphic.add(medicalAdministration()).repaint();
-				}else {
-					graphic.add(medicalAdministration()).repaint();
-				}
+				graphic.remove(subG);
+				graphic.remove(subG3);	
+				graphic.remove(subG4);	
+				graphic.remove(subG5);	
+				graphic.remove(subG6);		
+				medicalAdministration();
+				
+				
 			}
 		});;
 		
@@ -142,7 +153,12 @@ public class MainGraphicInterface {
 			
 			public void actionPerformed(ActionEvent arg0) {
 				
-				
+				graphic.remove(subG);
+				graphic.remove(subG2);	
+				graphic.remove(subG4);	
+				graphic.remove(subG5);	
+				graphic.remove(subG6);	
+				medicalServicesAdministration();
 				
 			}
 		});;
@@ -152,6 +168,12 @@ public class MainGraphicInterface {
 		
 			public void actionPerformed(ActionEvent arg0) {
 				
+				graphic.remove(subG);
+				graphic.remove(subG2);	
+				graphic.remove(subG3);	
+				graphic.remove(subG5);	
+				graphic.remove(subG6);	
+				administrationScheduleAppointments();
 				
 			}
 		});;
@@ -161,6 +183,12 @@ public class MainGraphicInterface {
 		
 			public void actionPerformed(ActionEvent arg0) {
 				
+				graphic.remove(subG);
+				graphic.remove(subG2);	
+				graphic.remove(subG3);	
+				graphic.remove(subG4);	
+				graphic.remove(subG6);	
+				serviceBilling();
 				
 			}
 		});;
@@ -169,8 +197,12 @@ public class MainGraphicInterface {
 			
 			
 			public void actionPerformed(ActionEvent arg0) {
-				
-				
+				graphic.remove(subG2);
+				graphic.remove(subG3);	
+				graphic.remove(subG4);	
+				graphic.remove(subG5);	
+				graphic.remove(subG);	
+				about();
 			}
 		});;
 		
@@ -191,11 +223,11 @@ public class MainGraphicInterface {
 		
 	}
 	
-	private JPanel customerAdministration() {
+	private void customerAdministration() {
 		
-		subG = new JPanel();
+		
 		subG.setLayout(null);
-		subG.setBackground(Color.white);
+		subG.setBackground(Color.GRAY);
 		subG.setBounds(320, 10, 515, 400);
 		
 		JButton b1 = new JButton("Add");
@@ -204,7 +236,7 @@ public class MainGraphicInterface {
 		JButton b4 = new JButton("Remove");
 		JButton b5 = new JButton("List");
 	
-		b1.setBackground(Color.BLUE);
+		b1.setBackground(Color.white);
 		b2.setBackground(Color.white);
 		b3.setBackground(Color.white);
 		b4.setBackground(Color.white);
@@ -216,29 +248,31 @@ public class MainGraphicInterface {
 		b4.setBounds(10, 190, 100, 40);
 		b5.setBounds(10, 250, 100, 40);
 	
-		
 		subG.add(b1);
 		subG.add(b2);
 		subG.add(b3);
 		subG.add(b4);
 		subG.add(b5);
 		
+	
+		
 		subG.setVisible(true);
 		
-		return subG;
+		graphic.add(subG).repaint();
+		
+	
 	}
 	
-private JPanel medicalAdministration() {
+private void medicalAdministration() {
+	
+		subG2.setLayout(null);
+		subG2.setBackground(Color.GRAY);
+		subG2.setBounds(320, 10, 515, 400);
 		
-		subG = new JPanel();
-		subG.setLayout(null);
-		subG.setBackground(Color.pink);
-		subG.setBounds(320, 10, 515, 400);
-		
-		JButton b1 = new JButton("Adfdd");
-		JButton b2 = new JButton("Seadfrch");
-		JButton b3 = new JButton("Modfdify");
-		JButton b4 = new JButton("Remdfove");
+		JButton b1 = new JButton("Add");
+		JButton b2 = new JButton("Search");
+		JButton b3 = new JButton("Modify");
+		JButton b4 = new JButton("Remove");
 		JButton b5 = new JButton("List");
 	
 		b1.setBackground(Color.white);
@@ -254,27 +288,139 @@ private JPanel medicalAdministration() {
 		b5.setBounds(10, 250, 100, 40);
 	
 		
-		subG.add(b1);
-		subG.add(b2);
-		subG.add(b3);
-		subG.add(b4);
-		subG.add(b5);
+		subG2.add(b1);
+		subG2.add(b2);
+		subG2.add(b3);
+		subG2.add(b4);
+		subG2.add(b5);
+
+		subG2.setVisible(true);
 		
-		
-		return subG;
-		
+		graphic.add(subG2).repaint();	
 		
 	}
 	
-	private boolean panelOpen(JPanel i) {
+	 private void medicalServicesAdministration() {
+		 
+		 	subG3.setLayout(null);
+			subG3.setBackground(Color.GRAY);
+			subG3.setBounds(320, 10, 515, 400);
+			
+			JButton b1 = new JButton("Add");
+			JButton b3 = new JButton("Modify");;
+			JButton b5 = new JButton("List");
 		
-		boolean open = false;
+			b1.setBackground(Color.white);
+			b3.setBackground(Color.white);
+			b5.setBackground(Color.white);
+
+			b1.setBounds(10, 10, 100, 40);
+			b3.setBounds(10, 130, 100, 40);
+			b5.setBounds(10, 250, 100, 40);
 		
-		if(i.isVisible()) {
-			open =  true;
-		}
+			
+			subG3.add(b1);
+			subG3.add(b3);
+			subG3.add(b5);
+
+			subG3.setVisible(true);
+			
+			graphic.add(subG3).repaint();	
+		 
+		 
+	 }
+	 
+	 private void administrationScheduleAppointments() {
+		 	subG4.setLayout(null);
+			subG4.setBackground(Color.GRAY);
+			subG4.setBounds(320, 10, 515, 400);
+			
+			JButton b1 = new JButton("Developing");
+			JButton b2 = new JButton("Developing");
+			JButton b3 = new JButton("Developing");
+			JButton b4 = new JButton("Developing");
+			JButton b5 = new JButton("Developing");
 		
-		return open;
-	}
+			b1.setBackground(Color.white);
+			b2.setBackground(Color.white);
+			b3.setBackground(Color.white);
+			b4.setBackground(Color.white);
+			b5.setBackground(Color.white);
+
+			b1.setBounds(10, 10, 100, 40);
+			b2.setBounds(10, 70, 100, 40);
+			b3.setBounds(10, 130, 100, 40);
+			b4.setBounds(10, 190, 100, 40);
+			b5.setBounds(10, 250, 100, 40);
+		
+			
+			subG4.add(b1);
+			subG4.add(b2);
+			subG4.add(b3);
+			subG4.add(b4);
+			subG4.add(b5);
+
+			subG4.setVisible(true);
+			
+			graphic.add(subG4).repaint();	
+	 }
 	
+	 private void serviceBilling() {
+		 	subG5.setLayout(null);
+			subG5.setBackground(Color.GRAY);
+			subG5.setBounds(320, 10, 515, 400);
+			
+			JButton b1 = new JButton("Developing");
+			JButton b2 = new JButton("Developing");
+			JButton b3 = new JButton("Developing");
+			JButton b4 = new JButton("Developing");
+			JButton b5 = new JButton("Developing");
+		
+			b1.setBackground(Color.white);
+			b2.setBackground(Color.white);
+			b3.setBackground(Color.white);
+			b4.setBackground(Color.white);
+			b5.setBackground(Color.white);
+
+			b1.setBounds(10, 10, 100, 40);
+			b2.setBounds(10, 70, 100, 40);
+			b3.setBounds(10, 130, 100, 40);
+			b4.setBounds(10, 190, 100, 40);
+			b5.setBounds(10, 250, 100, 40);
+		
+			
+			subG5.add(b1);
+			subG5.add(b2);
+			subG5.add(b3);
+			subG5.add(b4);
+			subG5.add(b5);
+
+			subG5.setVisible(true);
+			
+			graphic.add(subG5).repaint();	
+	 }
+	 
+	 private void about() {
+		 
+		 	subG6.setLayout(null);
+			subG6.setBackground(Color.white);
+			subG6.setBounds(320, 10, 515, 400);
+			
+			JLabel txt = new JLabel();
+			String txt_ = "Developed \n by Anthony Marin Bolivar and Erick Badilla"
+					+ "\n Version:1.3 DEVELOPINGGGGG";
+			txt.setText(txt_);
+			txt.setLayout(null);
+			txt.setBounds(10, 10, 400, 400);
+			
+			
+			subG6.add(txt);
+
+			subG6.setVisible(true);
+			
+			graphic.add(subG6).repaint();	
+		 
+		 
+	 }
+	 
 }
