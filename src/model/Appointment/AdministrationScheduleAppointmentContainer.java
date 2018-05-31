0f25eec4f22f113b24ebfaaa73131ab3d4c40869
william arias <willi.am@hotmail.es>
2018-05-30 21:40:33 -0600

    Minor changes Appointment 0.2

--- src/model/Appointment/AdministrationScheduleAppointmentContainer.java
+++ src/model/Appointment/AdministrationScheduleAppointmentContainer.java
@@ -2,4 +2,7 @@ package model.Appointment;
 
 public class AdministrationScheduleAppointmentContainer {
 
+	 public AdministrationScheduleAppointmentContainer() throws Exception{
+	        super("Appointment");
+	    }
 }
--- src/model/Appointment/Appointment.java
+++ src/model/Appointment/Appointment.java
@@ -2,4 +2,48 @@ package model.Appointment;
 
 public class Appointment {
 
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String date;
+	private String time;
+	private String numAppoint;
+	private String clientName;
+	private String doctor;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getNumAppoint() {
+		return numAppoint;
+	}
+	public void setNumAppoint(String numAppoint) {
+		this.numAppoint = numAppoint;
+	}
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public String getDoctor() {
+		return doctor;
+	}
+	public void setDoctor(String doctor) {
+		this.doctor = doctor;
+	}
+	
 }
