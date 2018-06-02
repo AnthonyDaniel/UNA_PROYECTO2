57b2e83eadcd918843c13c929c58c8a47d4da698
Erick Badilla <rvoltpwn@gmail.com>
2018-06-02 16:14:38 -0600

    Medical Services terminado

--- src/model/Services/MedicalServicesAdministration.java
+++
@@ -1,5 +0,0 @@
-package model.Services;
-
-public class MedicalServicesAdministration {
-
-}
--- src/model/Services/MedicalServicesAdministrationInterface.java
+++
@@ -1,5 +0,0 @@
-package model.Services;
-
-public interface MedicalServicesAdministrationInterface {
-
-}
